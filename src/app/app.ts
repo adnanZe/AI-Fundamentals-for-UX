6eea0a1adca75ee997344344e772de2f42c4ad0c
adnanZe <adnan.zecheria@yahoo.com>
2025-12-14 20:50:46 +0200

    #3 Augmentation over automation

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { WrongExampleComponent } from './components/wrong-example/wrong-example'
 import { RightExampleComponent } from './components/right-example/right-example';
 import { RecruitmentWrongComponent } from './components/recruitment-wrong/recruitment-wrong';
 import { RecruitmentRightComponent } from './components/recruitment-right/recruitment-right';
+import { SupportWrongComponent } from './components/support-wrong/support-wrong';
+import { SupportRightComponent } from './components/support-right/support-right';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +13,16 @@ import { RecruitmentRightComponent } from './components/recruitment-right/recrui
     RightExampleComponent,
     RecruitmentWrongComponent,
     RecruitmentRightComponent,
+    SupportWrongComponent,
+    SupportRightComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly selectedExample = signal<'mindset' | 'recruitment'>('mindset');
+  protected readonly selectedExample = signal<'mindset' | 'recruitment' | 'support'>('mindset');
 
-  protected selectExample(example: 'mindset' | 'recruitment'): void {
+  protected selectExample(example: 'mindset' | 'recruitment' | 'support'): void {
     this.selectedExample.set(example);
   }
 }
